39c93c44eb19abfd00613a9255230bc4b7346a46
Grey <sergejyurskyj@yahoo.com>
2020-05-23 21:59:19 -0700

    Committed 4. 3- Create Your First Python GUI Application using PyQT5

---
+++ VIDEO/Python_OpenCV_Based_GUI_Application_Development_using_PyQt5/Section 1 OpenCV GUI Development for Images/4. 3/Tutorial 3/main.py
@@ -0,0 +1,22 @@
+import sys
+
+import cv2
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+
+
+class Life2Coding(QDialog):
+    def __init__(self):
+        super(Life2Coding,self).__init__()
+        loadUi('life2coding.ui',self)
+
+
+app=QApplication(sys.argv)
+window=Life2Coding()
+window.setWindowTitle('Hanif PyQt5 Tutorials')
+window.setGeometry(100,100,400,200)
+window.show()
+sys.exit(app.exec_())
