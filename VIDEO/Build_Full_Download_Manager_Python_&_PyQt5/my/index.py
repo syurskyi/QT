4623691a61fd1ce2b766b059e9f788f02a6147f9
Grey <sergejyurskyj@yahoo.com>
2020-06-13 16:24:46 -0700

    Committed 6. Design App

--- VIDEO/Build_Full_Download_Manager_Python_&_PyQt5/my/index.py
+++ VIDEO/Build_Full_Download_Manager_Python_&_PyQt5/my/index.py
@@ -3,16 +3,14 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import sys
 
-import os
-import os.path
 from PyQt5.uic import loadUiType
 
-ui = loadUiType('main.ui')
+ui,_ = loadUiType('main.ui')
 
 class MainApp(QMainWindow, ui):
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
-        QMainWindow.__init__(parent)
+        QMainWindow.__init__(self)
         self.setupUi(self)
 
 
@@ -21,6 +19,9 @@ def main():
     window = MainApp()
     window.show()
     app.exec_()
+
+if __name__ == '__main__':
+    main()
 
 
 
