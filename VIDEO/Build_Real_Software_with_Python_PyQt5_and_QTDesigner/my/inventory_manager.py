083822f6c1a8dfd3e2d88737fa57ba9420d73de9
Grey <sergejyurskyj@yahoo.com>
2020-06-15 17:30:46 -0700

    Committed 10. Programming Part 3 - Create Search Filter for Parts Inventory Count

--- VIDEO/Build_Real_Software_with_Python_PyQt5_and_QTDesigner/my/inventory_manager.py
+++ VIDEO/Build_Real_Software_with_Python_PyQt5_and_QTDesigner/my/inventory_manager.py
@@ -26,6 +26,7 @@ class Main(QMainWindow, FORM_CLASS):
 
     def handle_buttons(self):
         self.refresh_btn.clicked.connect(self.get_data)
+        self.search_btn.clicked.connect(self.search)
 
     def get_data(self):
         # Connect to Sqlite3 database add fill GUI table with
@@ -38,6 +39,23 @@ class Main(QMainWindow, FORM_CLASS):
         
         self.table.setRowCount(0)
         
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+    def search(self):
+
+        db = sqlite3.connect("parts.db")
+        cursor = db.cursor()
+        nbr = int(self.count_filter_txt.text())
+
+        command = """SELECT * from parts_table WHERE count <= ?"""
+
+        result = cursor.execute(command, [nbr])
+
+        self.table.setRowCount(0)
+
         for row_number, row_data in enumerate(result):
             self.table.insertRow(row_number)
             for column_number, data in enumerate(row_data):
