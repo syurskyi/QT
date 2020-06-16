116a8fbab029c66ec054ee941e5dc75aad1bbe44
Grey <sergejyurskyj@yahoo.com>
2020-06-15 17:15:04 -0700

    Committed 8. Programming Part 1 - Create PyQt GUI loading and

---
+++ VIDEO/Build_Real_Software_with_Python_PyQt5_and_QTDesigner/my/inventory_manager.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QApplication
+
+import sys
+from os import path
+
+from PyQt5.uic import loadUiType
+
+
+FORM_CLASS,_ = loadUiType(path.join(path.dirname('__file__'), "main.ui"))
+
+import sqlite3
+
+
+class Main(QMainWindow, FORM_CLASS):
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.handle_buttons()
+
+    def handle_buttons(self):
+        pass
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
+
