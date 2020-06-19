f1f10f54dc4aef51e1c72c2ac8e79284ca1b1af9
Grey <sergejyurskyj@yahoo.com>
2020-06-18 17:55:10 -0700

    Committed Section 17: Stylesheets

--- VIDEO/Python_Tool_Development_with_PySide&PyQt/my/launcher_view.py
+++ VIDEO/Python_Tool_Development_with_PySide&PyQt/my/launcher_view.py
@@ -15,6 +15,39 @@ import launcher_model
 PyQt5_VER = PYQT_VERSION_STR
 PY_VER = sys.version[:3]
 
+_STYLE_MAIN = '''
+    QWidget{
+        color: white; 
+        background-color: qlineargradient(
+            x1: 0, y1: 0, x2: 1, y2: 1,
+            stop: 0 #052e32, stop: 0.25 #030f00,
+            stop: 0.75 #030f00, stop: 1 #382213);
+        alternate-background-color: #072100;
+    } 
+    QComboBox{ 
+        color: white;
+        background: black;
+        border: 1px inset gray;
+        border-radius: 2px;
+    }
+    QGroupBox{ background-color: rgba(0,0,0,0)}
+    QLineEdit{ color: white;  background-color: rgb(0,0,0); }
+    '''
+_STYLE_BTN = '''
+    QPushButton{ 
+        border: 1px outset gray;
+        border-radius: 2px;
+        background-color: #00351d;
+        min-width: 40px;
+    }
+    QPushButton:checked { 
+        border: 1px outset gray;
+        border-radius: 2px;
+        background-color: #701200;
+        min-width: 40px;
+    } 
+    '''
+
 
 class Delete_Btn(QtWidgets.QPushButton):
     ''' Custom button that Triggers parent's 'delete_item' function. '''
@@ -23,18 +56,18 @@ class Delete_Btn(QtWidgets.QPushButton):
         self._my_parent = parent
         self._type = 'application/x-qabstractitemmodeldatalist'
 
-    def dragEnterEvent(self, e):
-        if e.mimeData().hasFormat(self._type):
-            e.accept()
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasFormat(self._type):
+            event.accept()
         else:
-            e.ignore()
+            event.ignore()
 
-    def dropEvent(self, e):
-        if e.mimeData().hasFormat(self._type):
+    def dropEvent(self, event):
+        if event.mimeData().hasFormat(self._type):
             self._my_parent.delete_item()
-            e.accept()
+            event.accept()
         else:
-            e.ignore()
+            event.ignore()
 
 
 class LauncherGUI(QtWidgets.QWidget):
@@ -45,19 +78,24 @@ class LauncherGUI(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(LauncherGUI, self).__init__(parent)
 
+        # System and tool icons
         self._icons = QtWidgets.QFileIconProvider()
         self._my_path = os.path.dirname(os.path.realpath(__file__))
         images_path = os.path.join(self._my_path, 'images')
         full_path = os.path.join(images_path, 'launcher.png')
         self._tpl_icon = QtGui.QIcon(full_path)
 
+        # Data holding instance
         self._launcher = launcher_model.LauncherModel()
 
+        # Toggle dictionary to replace 'if/else' statements
         self._edit_op = {
             True: self._edit_on,
             False: self._edit_off
         }
         self._app_instruction = True
+
+        # Handle version specific file dialog box issues
         self._file_dialogs = defaultdict(lambda: self._file_dialog)
         self._file_dialogs['PyQt5'] = self._file_dialog_pyqt5
         self._dragging = None
@@ -66,16 +104,19 @@ class LauncherGUI(QtWidgets.QWidget):
             'application': self._delete_app
         }
 
+        # Settings and json file read/write stuf
         txt = 'SYurskyi_Experience' + PyQt5_VER
         self._settings = QtCore.QSettings(txt, 'SYurskyi\'s_Launcher')
         name = 'Python-{}_PyQt5-{}_data.json'.format(PY_VER, PyQt5_VER)
         path = os.path.join(self._my_path, 'data')
         self._json_file = os.path.join(path, name)
 
+        # Handle version specific reading/writing settings
         d = defaultdict(lambda: self._get_settings)
         self._settings_op = defaultdict(lambda: d)
         self._settings_op['2.7']['PyQt4'] = self._get_settings_27_pyqt4
 
+        # Setup GUI, then load settings, then display editmode properly
         self._setup()
         self._load_settings()
         self._edit_toggle()
@@ -83,6 +124,11 @@ class LauncherGUI(QtWidgets.QWidget):
     # ================ SETUP UI ========================================================================================
 
     def _setup(self):
+        """
+        Create tool GUI, set title, icon, remove
+        minimize / maximize buttons, resize and style.
+        """
+
         v_layout = QtWidgets.QVBoxLayout(self)
 
         v_layout.addLayout(self._setup_header())
@@ -97,19 +143,21 @@ class LauncherGUI(QtWidgets.QWidget):
         flag = QtCore.Qt.WindowCloseButtonHint
         self.setWindowFlags(QtCore.Qt.Window | flag)
         self.resize(190, 200)
+        self.setStyleSheet(_STYLE_MAIN)
 
     def _setup_header(self):
+        """Create combobox for workspace and edit button"""
         h_layout = QtWidgets.QHBoxLayout()
         workspace_gb = QtWidgets.QGroupBox('Workspaces')
         v_layout = QtWidgets.QVBoxLayout(workspace_gb)
 
         self._workspace_cb = QtWidgets.QComboBox()
-        flag = QtWidgets.QAbstractItemView.DragOnly
-        self._workspace_cb.view().setDragDropMode(flag)
+        self._workspace_cb.view().setDragDropMode(QtWidgets.QAbstractItemView.DragOnly)
 
         self._edit_btn = QtWidgets.QPushButton('Edit')
         self._edit_btn.setMaximumSize(QtCore.QSize(40, 23))
         self._edit_btn.setCheckable(True)
+        self._edit_btn.setStyleSheet(_STYLE_BTN)
 
         v_layout.addWidget(self._workspace_cb)
         h_layout.addWidget(workspace_gb)
@@ -118,6 +166,7 @@ class LauncherGUI(QtWidgets.QWidget):
         return h_layout
 
     def _setup_edit_options(self):
+        """Create add / delete buttons with menus and overrides"""
         add_del_l = QtWidgets.QHBoxLayout()
 
         self._add_btn = QtWidgets.QPushButton('Add')
@@ -139,13 +188,14 @@ class LauncherGUI(QtWidgets.QWidget):
         return add_del_l
 
     def _setup_apps(self):
+        """Create apps list widget with drag/drop, and stuff"""
         self._app_lw = QtWidgets.QListWidget()
         self._app_lw.setAlternatingRowColors(True)
-        flag = QtWidgets.QAbstractItemView.InternalMove
-        self._app_lw.setDragDropMode(flag)
+        self._app_lw.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
         return self._app_lw
 
     def _setup_tray_icon(self):
+        """Create icon for system tray and actions menu"""
         tray_icon = QtWidgets.QSystemTrayIcon(self)
         tray_icon.setIcon(self._tpl_icon)
         tray_icon.setToolTip('Launches other apps!')
@@ -154,35 +204,34 @@ class LauncherGUI(QtWidgets.QWidget):
         tray_menu.addAction('Open', self.show)
         tray_menu.addAction('YouTube', self._launcher.run_youtube)
         tray_menu.addSeparator()
-        q = QtWidgets.QAction('Quit', self, triggered=self._close)
-        tray_menu.addAction(q)
+        tray_menu.addAction(QtWidgets.QAction('Quit', self, triggered=self._close))
         tray_icon.setContextMenu(tray_menu)
         tray_icon.show()
 
     def _setup_connections(self):
-        ws = self._workspace_changed
-        self._workspace_cb.currentIndexChanged.connect(ws)
+        """Create connections between combobox, apps, edit to do stuff"""
+        self._workspace_cb.currentIndexChanged.connect(self._workspace_changed)
         self._edit_btn.clicked.connect(self._edit_toggle)
-        dw = self._dragging_workspace
-        self._workspace_cb.view().pressed.connect(dw)
+        self._workspace_cb.view().pressed.connect(self._dragging_workspace)
         self._app_lw.itemPressed.connect(self._dragging_app)
 
     # ================ DISPLAY ========================================================================================
 
     def _populate_workspaces(self):
-        ws = self._workspace_changed
-        self._workspace_cb.currentIndexChanged.disconnect(ws)
+        """Clear and Populate the workspace combobox, disconnect and reconnect signal to avoid unintended refresh of populate apps."""
+        self._workspace_cb.currentIndexChanged.disconnect(self._workspace_changed)
         self._workspace_cb.clear()
         self._workspace_cb.addItems(self._launcher.get_workspaces())
-        self._workspace_cb.currentIndexChanged.connect(ws)
+        self._workspace_cb.currentIndexChanged.connect(self._workspace_changed)
         self._populate_apps()
 
     def _populate_apps(self):
+        """Clear and Populate the apps list widget, set each application icon and name."""
         self._app_lw.clear()
-        ws = self.get_workspace()
-        for app_name in self._launcher.get_app_names(ws):
+        workspace = self.get_workspace()
+        for app_name in self._launcher.get_app_names(workspace):
             item = QtWidgets.QListWidgetItem(self._app_lw)
-            icon = self._launcher.get_app_icon(ws, app_name)
+            icon = self._launcher.get_app_icon(workspace, app_name)
             if icon:
                 item.setIcon(QtGui.QIcon(icon))
             else:
@@ -190,10 +239,12 @@ class LauncherGUI(QtWidgets.QWidget):
             item.setText(app_name)
 
     def closeEvent(self, e):
+        """Override Qt close event to instead hide tool"""
         self.hide()
         e.ignore()
 
     def _close(self):
+        """ Save settings and Quit tool """
         self._save_settings()
         QtWidgets.QApplication.instance().quit()
 
@@ -214,6 +265,7 @@ class LauncherGUI(QtWidgets.QWidget):
     # ================ EDIT MODE ======================================================================================
 
     def _edit_toggle(self):
+        """Whenever button is clicked, set add/del button vis, set drag drop and connect/disconnect run apps signal"""
         self._edit_op[self._edit_btn.isChecked()]()
         self._app_lw.setDragEnabled(self._edit_btn.isChecked())
         self._add_btn.setVisible(self._edit_btn.isChecked())
@@ -231,9 +283,8 @@ class LauncherGUI(QtWidgets.QWidget):
     # ================ ADD ============================================================================================
 
     def _add_workspace(self):
-        ws = 'New WORKSPACE'
-        msg = 'What would you like to title the new WORKSPACE?'
-        text = QtWidgets.QInputDialog.getText(self, ws, msg)[0]
+        """Prompt user with line edit input dialog for new name, store if valid, populate, then select it."""
+        text = QtWidgets.QInputDialog.getText(self, 'New WORKSPACE', 'What would you like to title the new WORKSPACE?')[0]
         if text:
             self._launcher.add_workspace(str(text))
             self._populate_workspaces()
@@ -242,6 +293,8 @@ class LauncherGUI(QtWidgets.QWidget):
             self._save_settings()
 
     def _add_app(self):
+        """Present user with instructions ONCE, then manditory file dialog for app, optional dialog for icon,
+        store, populate, and save settings"""
         if self._app_instruction:
             msg = '1) Select the application file\n'
             msg += '2) Select a app icon image (Optional)'
@@ -258,19 +311,24 @@ class LauncherGUI(QtWidgets.QWidget):
             self._save_settings()
 
     def _file_dialog(self, msg, path):
+        """Default File dialog for Pyside, Pyside2, PyQt5 return file path and filter for the dialog.
+        Only need the path"""
         return QtWidgets.QFileDialog.getOpenFileName(self, msg, path)[0]
 
     def _file_dialog_pyqt5(self, msg, path):
+        """File dialog for PyQt4 only returns file path as QString, must be converted to a string"""
         fd = QtWidgets.QFileDialog.getOpenFileName(self, msg, path)
         return str(fd)
 
     # ================ DELETE =========================================================================================
 
     def _dragging_workspace(self, index):
+        """When the user begins dragging a workspace, store the index of the item being dragged and note that it is a workspace."""
         ws = str(self._workspace_cb.itemText(index.row()))
         self._dragging = ('workspace', ws)
 
     def _dragging_app(self, item):
+        """When the user begins dragging a application, store the item text and note it's an app"""
         self._dragging = ('application', str(item.text()))
 
     def _delete_app(self, app_name):
@@ -296,16 +354,18 @@ class LauncherGUI(QtWidgets.QWidget):
     # ================ SETTING =========================================================================================
 
     def _save_settings(self):
-        c = self._app_lw.count()
-        names = [self._app_lw.item(i).text() for i in range(c)]
-        ws = self.get_workspace()
-        self._launcher.reorder_apps(ws, names)
-        self._settings.setValue('CurrentWorkspace', ws)
+        """Reorder the apps if necessary, then save the user settings to QSettings, and store the tool data to json file."""
+        count = self._app_lw.count()
+        names = [self._app_lw.item(i).text() for i in range(count)]
+        workspace = self.get_workspace()
+        self._launcher.reorder_apps(workspace, names)
+        self._settings.setValue('CurrentWorkspace', workspace)
         self._launcher.write_json_file(self._json_file)
         self._settings.setValue('PosX', int(self.x()))
         self._settings.setValue('PosY', int(self.y()))
 
     def _load_settings(self):
+        """If there is a QSettings file and the json file exists, read in the json file, then make selections based on the qsettings."""
         if 'CurrentWorkspace' in self._settings.allKeys() and \
               os.path.exists(self._json_file):
             x, y, cws = self._settings_op[PY_VER][PyQt5_VER]()
@@ -315,16 +375,18 @@ class LauncherGUI(QtWidgets.QWidget):
             self._workspace_cb.setCurrentIndex(index)
             self.move(x,y)
         else:
-            self._tp_launcher.add_workspace('Default_WS')
+            self._launcher.add_workspace('Default_WS')
             self._populate_workspaces()
 
     def _get_settings(self):
+        """Default get settings for x, y position for, tool and current workspace"""
         x = int(self._settings.value('PosX')) #PyQt5 conversion
         y = int(self._settings.value('PosY'))
         cws = str(self._settings.value('CurrentWorkspace'))
         return x, y, cws
 
     def _get_settings_27_pyqt4(self):
+        """This specific version of PyQt and python needed to be handled differently."""
         x = self._settings.value('PosX').toInt()[0]
         y = self._settings.value('PosY').toInt()[0]
         cws = str(self._settings.value('CurrentWorkspace'))
@@ -332,16 +394,22 @@ class LauncherGUI(QtWidgets.QWidget):
 
 
 if __name__ == '__main__':
+    # for debugging
     print(PY_VER)
     print(PyQt5_VER)
     app = QtWidgets.QApplication(sys.argv)
+
+    # if os doesn't support Tray, quit
     if not QtWidgets.QSystemTrayIcon.isSystemTrayAvailable():
         QtWidgets.QMessageBox.critical(
             None, 'Error!',
             'No system tray available for this system! Exiting...'
         )
         sys.exit(1)
+    # do not close if tool is hidden
     QtWidgets.QApplication.setQuitOnLastWindowClosed(False)
+
+    # Create an instance of tool and run i
     ex = LauncherGUI()
     ex.show()
     sys.exit(app.exec_())
